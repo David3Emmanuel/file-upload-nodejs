baf7ea952b56da65e37f56d4098c05e35a1338ed
David Emmanuel <david3emmanuel@gmail.com>
2024-10-18 23:09:35 +0100

    feat: add route for getting files and metadata

--- src/routes/files.js
+++ src/routes/files.js
@@ -4,17 +4,46 @@ import fs from "fs/promises";
 const router = new Router();
 
 router.get("/", (req, res) => {
-  // Get all files in the uploads directory
-  fs.readdir("./uploads")
-    .then((files) => {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ files }));
-    })
-    .catch((err) => {
-      console.error(err);
-      res.writeHead(500, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "Internal Server Error" }));
-    });
+  // If file query parameter is provided, return the file
+  if (req.query.has("file")) {
+    const fileName = req.query.get("file");
+
+    Promise.all([
+      fs.readFile("./uploads/.meta.json"),
+      fs.readFile(`./uploads/${fileName}`),
+    ])
+      .then(([allMetadata, data]) => {
+        const metadata = JSON.parse(allMetadata)[fileName];
+        if (!metadata) throw new Error("File Not Found");
+        res.writeHead(200, { "Content-Type": metadata.contentType });
+        res.end(data);
+      })
+      .catch((err) => {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(
+          JSON.stringify({ error: "File Not Found", message: err.message })
+        );
+      });
+    // Get all files in the uploads directory
+  } else {
+    fs.readdir("./uploads")
+      .then((files) => {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ files }));
+      })
+      .catch((err) => {
+        console.error(err);
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Internal Server Error" }));
+      });
+  }
+});
+
+router.get("/metadata", (req, res) => {
+  fs.readFile("./uploads/.meta.json").then((metadata) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(metadata);
+  });
 });
 
 export default router;
