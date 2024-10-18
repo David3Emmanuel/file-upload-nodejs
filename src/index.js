0f422d2fcdec1f79a863905a3915625f29ea6817
David Emmanuel <david3emmanuel@gmail.com>
2024-10-18 18:39:48 +0100

    feat: handle file uploads in POST requests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
+import fs from "fs/promises";
 import Server from "./Server.js";
 
 const server = new Server();
 
-server.get("/hello", (req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "Hello, World!" }));
+server.post("/", (req, res) => {
+  const { name, file } = req.body;
+  if (!file) {
+    res.writeHead(400, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "No file uploaded" }));
+    return;
+  }
+  fs.writeFile(`./uploads/${name || file.fileName}`, file.contents, {
+    mode: 0o765,
+  })
+    .then(() => {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({ message: "Uploaded", ...file, contents: undefined })
+      );
+    })
+    .catch((err) => {
+      res.writeHead(500, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: err.message }));
+    });
 });
 
 server.start(3001);
